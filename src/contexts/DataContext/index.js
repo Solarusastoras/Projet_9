ab0df890a2ab1d51f556a1ebd5fdd0e7330dee0c
Solarusastoras <micformweb@gmail.com>
2024-09-20 23:46:39 +0200

    Last event Fonctionelle

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -5,6 +5,9 @@ import {
   useContext,
   useEffect,
   useState,
+  // Ajout de useMemo pour last
+  // Add useMemo for last
+  useMemo,
 } from "react";
 
 const DataContext = createContext({});
@@ -19,6 +22,29 @@ export const api = {
 export const DataProvider = ({ children }) => {
   const [error, setError] = useState(null);
   const [data, setData] = useState(null);
+
+  // Utilisation de useMemo pour afficher le dernier évènement
+  // Using useMemo to display the latest event
+  const last = useMemo(() => {
+    // Vérifie si 'data' ou 'data.events' est nul ou indéfini
+    // Checks if 'data' or 'data.events' is null or undefined
+    if (!data || !data.events) return null;
+
+    // Utilise la méthode 'reduce' pour trouver l'événement le plus récent
+    // Uses the 'reduce' method to find the most recent event
+    return data.events.reduce(
+      (mostRecent, event) =>
+        // Si 'mostRecent' est nul ou si la date de l'événement actuel est plus récente que celle de 'mostRecent'
+        // If 'mostRecent' is null or if the current event's date is more recent than 'mostRecent'
+        !mostRecent || new Date(event.date) > new Date(mostRecent.date)
+          ? event
+          : mostRecent,
+      null
+    );
+    // Recalcule uniquement lorsque 'data' change
+    // Recomputes only when 'data' changes
+  }, [data]);
+
   const getData = useCallback(async () => {
     try {
       setData(await api.loadData());
@@ -30,13 +56,16 @@ export const DataProvider = ({ children }) => {
     if (data) return;
     getData();
   });
-  
+
   return (
     <DataContext.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
         data,
         error,
+        //Ajout de la valeur last
+        //Add value of last
+        last,
       }}
     >
       {children}
@@ -46,7 +75,7 @@ export const DataProvider = ({ children }) => {
 
 DataProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
 export const useData = () => useContext(DataContext);
 
