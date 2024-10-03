32e479d3e217acdf12e382310c5bf62735361829
Solarus <161462122+Solarusastoras@users.noreply.github.com>
2024-10-03 16:21:12 +0200

    Update Home  index.test.js
     remplacer par ce test 
    it("a list of events is displayed", async () => {
        render(<Home />);
        const events = screen.getAllByRole("listitem");
        expect(events.length).toBeGreaterThan(0);
      });

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,31 +1,6 @@
 import React from "react";
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen} from '@testing-library/react';
 import Home from "./index";
-
-// Mock des données d'événements
-const mockEvents = [
-  {
-    id: 1,
-    type: "conférence",
-    date: "2022-04-29T20:28:45.744Z",
-    title: "User&product MixUsers",
-    cover: "/images/alexandre-pellaes-6vAjp0pscX0-unsplash.png",
-  },
-  {
-    id: 2,
-    type: "expérience digitale",
-    date: "2022-01-29T20:28:45.744Z",
-    title: "#DigitonPARIS",
-    cover: "/images/charlesdeluvio-wn7dOzUh3Rs-unsplash.png",
-  },
-  {
-    id: 3,
-    type: "conférence",
-    date: "2022-03-29T20:28:45.744Z",
-    title: "Conférence &co-responsable",
-    cover: "/images/chuttersnap-Q_KdjKxntH8-unsplash.png",
-  }
-];
 
 // Test fonctionnel
 // Décrit un groupe de tests pour la création du formulaire
@@ -65,19 +40,12 @@ describe("When Form is created", () => {
 // Ajout de tests pour les autres éléments de la page
 describe("When a page is created", () => {
   // Test pour vérifier que la liste des événements est affichée
-  it("a list of events is displayed", async () => {
-    // Rend le composant Home avec les données mockées
-    render(<Home events={mockEvents} />);
-
-    // Utilise waitFor pour attendre que la section soit rendue
-    await waitFor(() => {
-      const eventListSection = screen.getByTestId("eventListHome");
-      expect(eventListSection).toBeInTheDocument();
-      
-      // Affiche le nombre d'éléments dans la console
-      console.log("Nbr Event Test :", eventListSection.childElementCount);
-    });
+ it("a list of events is displayed", async () => {
+    render(<Home />);
+    const events = screen.getAllByRole("listitem");
+    expect(events.length).toBeGreaterThan(0);
   });
+
 
   // Test pour vérifier que la liste des personnes est affichée
   it("a list of people is displayed", async () => {
